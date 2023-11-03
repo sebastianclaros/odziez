fef8264b77a66e002f35ca37f4de380b51e25faf
Sebastian Claros <sclaros@gmail.com>
2023-11-03 13:17:56 -0300

    init

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+  site_name: 'Starter',
+  title: 'Odziez',
+  description: 'Tienda Odziez',
+  url: 'https://www.odziez.ar',
+  locale: 'es',
+  author: 'Colegio Labarden',
+  pagination_size: 5,
+};
